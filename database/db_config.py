d1897a5f258eaad73d15531a9a3db86e8726c8ad
callistoitsolutions <124126575+callistoitsolutions@users.noreply.github.com>
2026-03-17 13:21:25 +0530

    Update db_config.py

--- database/db_config.py
+++ database/db_config.py
@@ -1,11 +1,15 @@
+import streamlit as st
 from sqlalchemy import create_engine
 from urllib.parse import quote_plus
 
-DB_USER = "root"
-DB_PASSWORD = quote_plus("palash@123")  # IMPORTANT
-DB_HOST = "localhost"
-DB_PORT = "3306"
-DB_NAME = "Customer_Behavior_Analytics"
+# Read database credentials from Streamlit Secrets
+db = st.secrets["database"]
+
+DB_USER = db["user"]
+DB_PASSWORD = quote_plus(db["password"])
+DB_HOST = db["host"]
+DB_PORT = db["port"]
+DB_NAME = db["database"]
 
 engine = create_engine(
     f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
